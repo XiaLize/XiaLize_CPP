cf8f3e1f1203e80db1b0d71c378ec2d6759e9bce
XiaLize <3689408982@qq.com>
2025-05-10 14:05:32 +0800

    2025/5/10

--- MNOP/pingyishuju.cpp
+++ MNOP/pingyishuju.cpp
@@ -9,13 +9,13 @@ int main()
     {
         cin >> a[i];
     }
-    for (int i = aa;i <= n;i--)
+    for (int i = aa-n+1;i <= aa;i++)
     {
         cout << a[i] << " ";
     }
-    /*for (int i = 1;i < n;i++)
+    for (int i = 1;i <= aa-n;i++)
     {
     cout << a[i] << " ";
-	}*/
+    }
     return 0;
 }
