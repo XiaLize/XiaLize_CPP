97f919e8ef6f530d3adb9d50d73b1356d73ad1d2
XiaLize <3689408982@qq.com>
2025-07-04 19:03:32 +0800

    2025/7/4

---
+++ asdfghjkl114514.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int h,m,s,k,sum,oh,om,os,temp;
+    cin >> h >> m >> s >> k;
+    sum += ((h*60*60) + (m*60) + s);
+    sum += k;
+    oh = sum /3600;
+    temp = sum - (oh * 3600);
+    om = (sum - temp) / 60;
+    temp -= om * 60;
+    os = temp;
+    cout << oh << " " << om << " " << os;
+}
