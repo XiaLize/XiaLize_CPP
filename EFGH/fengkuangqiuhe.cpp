7840aa7ba417f9153fa1e1dc3a40c1b2c1ffc6c9
XiaLize <3689408982@qq.com>
2025-07-09 20:46:15 +0800

    2025/7/9

--- EFGH/fengkuangqiuhe.cpp
+++ EFGH/fengkuangqiuhe.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main ()
 {
-    int n, m,f = 0;
+    long long n, m,f = 0;
     cin >> n >> m;
     int a[m];
     for (int i = 0; i < m; i++)
@@ -15,7 +15,7 @@ int main ()
         {
             if (a[i] + a[j] == n)
             {
-                cout << min(a[i], a[j]) << " " << max(a[i], a[j]) << endl;
+                cout << min(a[i], a[j]) << " " << max(a[i], a[j]);
                 f = 1;
             }
         }
