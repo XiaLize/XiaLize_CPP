32499557cd4fa3260bfe03fbd4a35c4f7289ebe6
XiaLize <3689408982@qq.com>
2026-02-05 17:22:55 +0800

    2/5

---
+++ QRST/tuxiangxuanzhuan.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define db double
+#define ll long long
+#define el endl
+#define r0 return 0
+#define IOS ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    int a[n + 1][m + 1];
+    for(int i = 1;i <= n;i++)
+    {
+        for(int j = 1;j <= m;j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for(int j = 1;j <= m;j++)
+    {
+        for(int i = n;i >= 1;i--)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << el;
+    }
+    r0;
+}
--- YZ/yanghuisanjiaoxing.cpp
+++ YZ/yanghuisanjiaoxing.cpp
@@ -5,8 +5,26 @@ using namespace std;
 #define el endl
 #define r0 return 0
 #define IOS ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+int a[20][20],n;
 int main()
 {
     
-    r0;
+    cin >> n;
+    a[1][1]=1; 
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+        	a[i][j]=a[i-1][j]+a[i-1][j-1];
+		}
+    }
+    for(int i=1;i<=n;i++)
+    {
+    	for(int j=1;j<=i;j++)
+    	{
+    		cout << a[i][j] << " ";
+		}
+		cout <<endl;
+		
+	}
 }
